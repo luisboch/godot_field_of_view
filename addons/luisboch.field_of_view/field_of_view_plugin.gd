cc4aa934b9463c17fb6b7de5811175e77f27c699
Luis Carlos Boch <lboch@celepar.pr.gov.br>
2020-05-16 22:18:31 -0300

    Icon and Node name changed

--- addons/luisboch.field_of_view/field_of_view_plugin.gd
+++ addons/luisboch.field_of_view/field_of_view_plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 var wind_singleton
 
 func _enter_tree():
-	add_custom_type("Field of View", "Node2D",\
+	add_custom_type("FieldOfView", "Node2D",\
 		load("res://addons/luisboch.field_of_view/field_of_view.gd"),\
 		load("res://addons/luisboch.field_of_view/node_icon.png"))
 func _exit_tree():
